3d0858c779a1a13513120972304641b3e2f3bcdd
Ewige前夕-Logic <Master@evec.cc>
2018-09-23 19:05:23 +0800

    提高了sql文件传输安全

--- AICM_Server.sh
+++ AICM_Server.sh
@@ -3,5 +3,5 @@ DengYu=$(date +%Y%m%d)
 DengYuriver=$(date +%Y-%m-%d,%H:%m)
 source /usr/bin/AICM/AICM.cfg
 
-wget -c http://$ipdy/$DengYu.sql -P $backupsave
+wget -c http://$ipdy/$DengYu.sql.tar.gz -P $backupsave
 wget -c http://$ipdy/$DengYu.tar.gz -P $backupsave
