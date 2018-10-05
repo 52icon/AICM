ac5e06824fadf474abc3a0053769d47fb2af92ba
Ewige前夕-Logic <Master@evec.cc>
2018-10-05 19:48:19 +0800

    新功能适配

--- AICM_Server.sh
+++ AICM_Server.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 DengYu=$(date +%Y%m%d)
 DengYuriver=$(date +%Y-%m-%d,%H:%m)
+speedu=k
 source /usr/bin/AICM/AICM.cfg
 
-wget -c http://$ipdy/$DengYu.sql.tar.gz -P $backupsave
-wget -c http://$ipdy/$DengYu.tar.gz -P $backupsave
+
+
+if [[ $limitrated == "yes" ]]; then
+    wget --limit-rate=$speedd$speedu -c http://$ipdy/$DengYu.sql.tar.gz -P $backupsave
+    wget --limit-rate=$speedd$speedu -c http://$ipdy/$DengYu.tar.gz -P $backupsave
+    else
+    wget -c http://$ipdy/$DengYu.sql.tar.gz -P $backupsave
+    wget -c http://$ipdy/$DengYu.tar.gz -P $backupsave
+fi
